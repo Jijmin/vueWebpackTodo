25ec03ce4dcd32c0313b00422548b72078ee7ca3
v_yinggzhou <v_yinggzhou@tencent.com>
2018-02-02 14:48:36 +0800

    webpack配置项目加载各种静态资源及css预处理器：css文件处理，css预处理器，图片处理

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@
 * @Author: v_yinggzhou
 * @Date:   2018-02-02 10:45:28
 * @Last Modified by:   v_yinggzhou
-* @Last Modified time: 2018-02-02 10:53:53
+* @Last Modified time: 2018-02-02 14:43:22
 */
 import Vue from 'vue'
 import App from './app.vue'
+
+import './assets/style/test.css'
+import './assets/style/test-stylus.styl'
+import './assets/images/bg.jpg'
 
 const root = document.createElement('div')
 document.body.appendChild(root)
--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,42 @@
 * @Author: v_yinggzhou
 * @Date:   2018-02-02 10:42:24
 * @Last Modified by:   v_yinggzhou
-* @Last Modified time: 2018-02-02 11:12:28
+* @Last Modified time: 2018-02-02 14:41:55
 */
 const path = require('path')
 
 module.exports = {
 	entry: path.join(__dirname, 'src/index.js'),// 输入
 	output: {// 输出
-		filename: 'bundle.js',
-		path: path.join(__dirname, 'dist')
+		filename: 'bundle.js',// 文件名
+		path: path.join(__dirname, 'dist')// 输出路径
 	},
 	module: {
-		rules: [{
-			test: /\.vue$/,
-			loader: 'vue-loader'
+		rules: [{// Vue文件的处理
+			test: /\.vue$/,// 匹配规则
+			loader: 'vue-loader'// 处理.vue的文件
+		}, {// css文件的处理
+			test: /\.css$/,
+			use: [
+				'style-loader',// 将读出来的css写到style标签中
+				'css-loader',//只是从css文件中将样式读取出来
+			]
+		}, {// css预处理器
+			test: /\.styl$/,
+			use: [
+				'style-loader',
+				'css-loader',
+				'stylus-loader'// 针对stylus文件做处理
+			]
+		}, {// 图片处理
+			test: /\.(gif|jpg|jpeg|png|svg)$/,
+			use: [{
+				loader: 'url-loader',// 小于1024的图片可以直接转换成base64代码
+				options: {
+					limit: 1024,
+					name: '[name]-zy.[ext]'// 保留原来的名字以及扩展名 
+				}
+			}]
 		}]
 	}
 }
